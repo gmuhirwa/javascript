f579f39a9bf079723fde75223d3336e838a565c7
Germain Muhirwa <gmuhirwa@yahoo.fr>
2020-01-19 14:48:16 -0500

    All files are updated.

--- Lecture53/js/script.js
+++ Lecture53/js/script.js
@@ -1,16 +1,39 @@
-// DOM manipulation
+// DOM manipulation --- Event Handling
+document.addEventListener("DOMContentLoaded", function (event) {
 
-function sayHello () {
-  var name = document.getElementById("name").value;
-  var message = "<h2>Hello " + name + "!</h2>"
+ function sayHello (event) {
+ /* console.log(this);*/
+ console.log(event);
+ this.textContent = "Said it!";
+ var name = document.getElementById("name").value;
+ var message = "<h2>Hello " + name + "!</h2>"
 
-  document.getElementById("content").textContent = message;
-
-  document.getElementById("content").innerHTML = message;
+ document.getElementById("content").textContent = message;
+ document.getElementById("content").innerHTML = message;
 
 if (name === "student") {
-   var title = document.querySelector("#title").textContent;
-   title += " & Lovin' it!";
-   document.querySelector("h1").textContent = title;  // h1 title is the only one can replace #title css!
+  var title = document.querySelector("#title").textContent;
+  title += " & Lovin' it!";
+  document.querySelector("h1").textContent = title;  // h1 title is the only one can replace #title css!
  }
 }
+
+// Unobstrusive event binding
+document.querySelector("button").addEventListener("click", sayHello);
+document.querySelector("body").addEventListener("mousemove",
+  function (event) {
+  	if (event.shiftKey === true ) {
+       console.log("x: " + event.clientX);
+     console.log("y: " + event.clientY);
+  	}
+     
+     }
+    );
+  }
+);
+
+
+
+/*// Action listner to a button
+document.querySelector("button").onclick = sayHello;
+*/
